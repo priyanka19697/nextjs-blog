f69a085da599a551fea9cc8f1f533c2006b39bf8
priyanka19697 <priyanka19697@gmail.com>
2021-05-03 09:07:48 +0530

    nextjs-blog

---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+// global css file
+import "../styles/global.css";
+export default function App({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ pages/posts/your-component.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+const YourComponent = () => (
+  <Image
+    src="/images/profile_pic.jpeg"
+    height={144}
+    width={144}
+    alt="priyanka"
+  ></Image>
+);
+
+export default YourComponent;
